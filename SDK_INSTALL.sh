f373788d2ec958989c87373ac4242d79f078fe76
Gol <hypergol@gmail.com>
2014-01-06 02:55:55 +0400

    Some fixes

--- SDK_INSTALL.sh
+++ SDK_INSTALL.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-sudo apt-get install git bash make flex bison gcc g++ gettext texinfo u-boot-tools ncurses-dev mercurial subversion awk > /dev/null
-make getsdk defconfig
+sudo apt-get -y install git bash make flex bison gcc g++ gettext texinfo u-boot-tools ncurses-dev mercurial subversion gawk libc6-i386 bc
+
+#make getsdk defconfig
