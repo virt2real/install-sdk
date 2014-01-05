7374bcd8cc02fe77c7ea9f897cf6e541d169f578
Gol <hypergol@gmail.com>
2014-01-06 02:57:32 +0400

    I'm stupid

--- SDK_INSTALL.sh
+++ SDK_INSTALL.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 sudo apt-get -y install git bash make flex bison gcc g++ gettext texinfo u-boot-tools ncurses-dev mercurial subversion gawk libc6-i386 bc
-
-#make getsdk defconfig
+make getsdk defconfig
