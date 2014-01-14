63f3c29ef2e08256503360c4e342e6ad37941581
Gol <root@virt2real.(none)>
2014-01-14 06:45:47 -0500

    Some changes in full update

--- FULL_UPDATE.sh
+++ FULL_UPDATE.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-#
-echo -e "\033[1;34mUpdate SDK scripts\033[0m"
+
+echo -e "\n\033[1mUpdate Virt2real SDK components\033[0m"
+
+echo -e "\n\033[1;34mUpdate SDK scripts\033[0m"
 git pull
-#
-echo -e "\033[1;34mUpdate SDK components\033[0m"
-#
-DIRS=`ls -d */` # or `cat .gitignore`
-#
+
+DIRS="adminka dvsdk fs kernel uboot"
 for dir in $DIRS;
 do
-	echo -e "\033[1;34mUpdate $dir\033[0m"
+	echo -e "\n\033[1;34mUpdate $dir\033[0m"
 	cd $dir && git pull
 	cd ..
 done
+
+echo -e "\n\033[1mupdate done\033[0m"
